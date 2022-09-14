61b2d7cab6bf2d6596692526d52a00e7a2db0c19
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-09-14 13:35:32 +0630

    refc: added components into barrel file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as ResponsiveDrawer } from "./ResponsiveDrawer";
+export { default as Appbar } from "./Appbar";
+export { default as CustomTable } from "./CustomTable";
+export { default as BackButton } from "./BackButton";
+export { default as DetailsRow } from "./DetailsRow";
+export { default as DeleteDialog } from "./DeleteDialog";
